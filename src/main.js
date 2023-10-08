4fbdf01d01a0962202edd59a1faa8c80b9214e94
Stevie Ambroise <s.ambroise@protonmail.com>
2023-10-07 20:45:01 -0600

    Increase accessibility of web app

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ let addBookButton = document.getElementById('add-button-el')
 let numberOfBooksRead = 0
 
 const addBook = () => {
-  shelfOne.innerHTML += `<div class=${randomizeBook()}></div>`
+  shelfOne.innerHTML += `<div class=${randomizeBook()} tabIndex="0"></div>`
   console.log('Book added!')
   numberOfBooksRead++
 }
@@ -11,6 +11,8 @@ const addBook = () => {
 
 const randomizeBook = () => {
   const books = ['purple', 'yellow', 'pink', 'red']
+
+  //blue, brown, turquoise, green, orange, lightblue, neongreen, white
 
   let index = Math.floor(Math.random() * books.length)
   return books[index]
